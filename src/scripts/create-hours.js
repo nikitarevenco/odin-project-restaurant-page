9f9a53e820620a65f343e6f8705e2f0067b53f84
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-01-26 01:01:51 +0000

    Refactor code

--- src/scripts/create-hours.js
+++ src/scripts/create-hours.js
@@ -1,11 +1,12 @@
 export default function (schedule) {
   const section = document.createElement("section");
-  section.setAttribute("id", "hours");
   const ul = document.createElement("ul");
   const h4 = document.createElement("h4");
+
+  section.id = "hours";
   h4.textContent = "Our schedule";
-  section.appendChild(h4);
-  section.appendChild(ul);
+
+  section.append(h4, ul);
   for (const [key, value] of Object.entries(schedule)) {
     const daySchedule = document.createElement("li");
     // MAJOR ASSUMPTION: Schedule starts in the morning and ends in the evening
